f5c656c549281a62e040358592272dd63455bb18
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-16 17:18:50 +0700

    feat: led

--- Core/Inc/game.h
+++ Core/Inc/game.h
@@ -4,14 +4,8 @@
 
 #include <stdint.h>
 
-unsigned int get_score();       // Hàm lấy điểm số hiện tại
-void reset_state();             // Hàm reset trạng thái điểm và led
+unsigned int get_score();               // Hàm lấy điểm số hiện tại
+void reset_state();                     // Hàm reset trạng thái điểm và led
 void press_button(uint8_t button);      // Hàm xử lý sự kiện nhấn button
 
-extern volatile uint8_t score_updated;  // Cờ báo cần cập nhật LCD
-
-void game_loop(uint32_t round_timeout); // Hàm chứa logic chính của game
-void pause_game(void);                  // Dừng random đèn (pause game)
-void resume_game(void);                 // Tiếp tục random đèn (resume game)
-
-#endif
+#endif  
