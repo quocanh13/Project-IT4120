9075c2dfa23101ae66519e40f4653f349525f202
quocanh13 <nguyenvan2308199@gmail.com>
2026-07-16 10:47:00 +0700

    feat: led

--- Core/Inc/led.h
+++ Core/Inc/led.h
@@ -15,7 +15,7 @@
 #define LED_6 GPIO_PIN_12
 
 void set_led(uint16_t led, uint8_t state);      // Thiết lập trạng thái bật tắt cho đèn LED (0 - 1)
-uint8_t get_led(uint16_t led);                  // Lấy trạng thái hiện tại của đèn LED (0 - 1)
+uint8_t get_led(uint8_t led_index);                  // Lấy trạng thái hiện tại của đèn LED (0 - 1)
 void rand_led(unsigned int level);              // Làm đèn LED nhảy ngẫu nhiên
 
 #define LCG_A  1664525U
